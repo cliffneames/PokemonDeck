a55aad44c5f949e12fc3591e86e724f9e0d48e49
cliff.neames <cliff.neames@gmail.com>
2017-05-03 14:38:28 -0500

    Add project files.

---
+++ PokemonDeck/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PokemonDeck.MvcApplication" Language="C#" %>
---
+++ PokemonDeck/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PokemonDeck.Startup))]
+namespace PokemonDeck
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
